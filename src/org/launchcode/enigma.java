31466bc3eb87e1026cd2dbb5cc7c034f4e783ef8
Cedzol <90751132+Cedzol@users.noreply.github.com>
2022-02-10 10:00:50 +0100

    Dial two and three

---
+++ src/org/launchcode/enigma.java
@@ -0,0 +1,19 @@
+package org.launchcode;
+
+
+public class enigma {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+
+    int[] dialone = {};
+
+    int[] dialtwo = {23, 9, 16, 19, 6, 5, 20, 26, 2, 13, 24, 15, 10, 25, 12, 3, 21, 22, 4, 7, 17, 18, 1, 11, 14, 8};
+
+    int[] dialthree = {8, 26, 18, 11, 20, 23, 13, 1, 25, 19, 4, 2, 7, 17, 25, 21, 14, 2, 10, 5, 16, 12, 6, 15, 9, 2};
+
+
+
+}
