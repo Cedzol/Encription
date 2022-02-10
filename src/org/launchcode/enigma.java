45388a55edc8a752cd50137f0cd2e3e57e7de2c4
Cedzol <90751132+Cedzol@users.noreply.github.com>
2022-02-10 12:15:43 +0100

    Give out character after changes

--- src/org/launchcode/enigma.java
+++ src/org/launchcode/enigma.java
@@ -13,13 +13,13 @@ public class enigma {
     public static void enigmaNumbers() {
 
 
-        int[] dialone = {17, 8, 21, 19, 20, 9, 16, 2, 6, 18, 25, 14, 22, 12, 26, 7, 1, 10, 4, 5, 3, 13, 24, 43, 11, 15};
+        int[] dialone = {17, 8, 21, 19, 20, 9, 16, 2, 6, 18, 25, 14, 22, 12, 26, 7, 1, 10, 4, 5, 3, 13, 24, 23, 11, 15};
 
         int[] dialtwo = {23, 9, 16, 19, 6, 5, 20, 26, 2, 13, 24, 15, 10, 25, 12, 3, 21, 22, 4, 7, 17, 18, 1, 11, 14, 8};
 
         int[] dialthree = {8, 26, 18, 11, 20, 23, 13, 1, 25, 19, 4, 2, 7, 17, 25, 21, 14, 2, 10, 5, 16, 12, 6, 15, 9, 2};
 
-        String character = "w";
+        String character = "b";
         
         String characterOut = null;
 
